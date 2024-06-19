e0eb9590e67f44b562585d9bd4810ee315a7d3a1
Dillon Wall <dillonmwall@msn.com>
2024-06-19 14:43:21 +0800

    Setting up clerk

--- gogosumo.client/src/app/layout.tsx
+++ gogosumo.client/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider"
+import Header from "./header"
 
 const fontSans = FontSans({
 	subsets: ["latin"],
@@ -19,8 +21,9 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+				<Header></Header>
 				{children}
 			</body>
 		</html>
