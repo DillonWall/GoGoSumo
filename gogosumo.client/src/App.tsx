3cdac60faa388ee8b2422f81a557997eaa79351d
Dillon Wall <dillonmwall@msn.com>
2024-07-02 01:01:13 +0800

    Add clerk integration

--- gogosumo.client/src/App.tsx
+++ gogosumo.client/src/App.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
 import RootLayout from "./components/layout"
 import { ModeToggle } from "./components/mode-toggle"
 import { Button } from "./components/ui/button"
@@ -10,6 +11,12 @@ function App() {
 				<main className="flex bg-background min-h-screen flex-col items-center justify-between p-24">
 					<Button>Click Me</Button>
 					<ModeToggle />
+					<SignedIn>
+						<UserButton afterSignOutUrl={window.location.href} />
+					</SignedIn>
+					<SignedOut>
+						<SignInButton mode="modal" />
+					</SignedOut>
 				</main>
 			</RootLayout>
 		</Providers>
--- gogosumo.client/src/providers.tsx
+++ gogosumo.client/src/providers.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from "@/components/theme-provider"
-// import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/clerk-react"
+
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY!
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
-			{/* <ClerkProvider> */}
-			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-				{children}
-			</ThemeProvider>
-			{/* </ClerkProvider> */}
+			<ClerkProvider publishableKey={publishableKey}>
+				<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+					{children}
+				</ThemeProvider>
+			</ClerkProvider>
 		</>
 	)
 }
