80e799962ffac96585d52766be97be5c1d640e82
Dillon Wall <dillonmwall@msn.com>
2024-07-24 17:30:09 +0800

    Prepare for UnitTests

---
+++ GoGoSumo.Server.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GoGoSumo.Server.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ GoGoSumo.Server.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GoGoSumo.Server.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
