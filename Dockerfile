c1c01844f2176919bdfd0b32870b7da3aefc53d3
Dillon Wall <dillonmwall@msn.com>
2024-08-05 15:54:14 +0800

    Restructure project and rework the Dockerfile and compose file in preparation for Azure container hosting

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,8 @@
 # Create a stage for building the application.
 FROM --platform=$BUILDPLATFORM mcr.microsoft.com/dotnet/sdk:8.0-alpine AS build
 
-COPY . /source
+COPY ./GoGoSumo.Server/ /source
+COPY ./GoGoSumo.DTOs/ /source
 
 WORKDIR /source
 
@@ -27,7 +28,7 @@ ARG TARGETARCH
 # If TARGETARCH is "amd64", replace it with "x64" - "x64" is .NET's canonical name for this and "amd64" doesn't
 #   work in .NET 6.0.
 RUN --mount=type=cache,id=nuget,target=/root/.nuget/packages \
-    dotnet publish -a ${TARGETARCH/amd64/x64} --use-current-runtime --self-contained false -o /app
+    dotnet publish GoGoSumo.Server.csproj -a ${TARGETARCH/amd64/x64} --use-current-runtime --self-contained false -o /app --source /source/GoGoSumo.Server/
 
 # If you need to enable globalization and time zones:
 # https://github.com/dotnet/dotnet-docker/blob/main/samples/enable-globalization.md
--- gogosumo.client/vite.config.ts
+++ gogosumo.client/vite.config.ts
@@ -3,39 +3,39 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import plugin from "@vitejs/plugin-react"
 import fs from "fs"
-import path from "path"
-import child_process from "child_process"
+// import path from "path"
+// import child_process from "child_process"
 
-const baseFolder =
-	process.env.APPDATA !== undefined && process.env.APPDATA !== ""
-		? `${process.env.APPDATA}/ASP.NET/https`
-		: `${process.env.HOME}/.aspnet/https`
+// const baseFolder =
+// 	process.env.APPDATA !== undefined && process.env.APPDATA !== ""
+// 		? `${process.env.APPDATA}/ASP.NET/https`
+// 		: `${process.env.HOME}/.aspnet/https`
 
-const certificateArg = process.argv.map((arg) => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0]
-const certificateName = certificateArg ? certificateArg.groups!.value : "gogosumo.client"
+// const certificateArg = process.argv.map((arg) => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0]
+// const certificateName = certificateArg ? certificateArg.groups!.value : "gogosumo.client"
 
-if (!certificateName) {
-	console.error(
-		"Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly."
-	)
-	process.exit(-1)
-}
+// if (!certificateName) {
+// 	console.error(
+// 		"Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly."
+// 	)
+// 	process.exit(-1)
+// }
 
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`)
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`)
+// const certFilePath = path.join(baseFolder, `${certificateName}.pem`)
+// const keyFilePath = path.join(baseFolder, `${certificateName}.key`)
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-	if (
-		0 !==
-		child_process.spawnSync(
-			"dotnet",
-			["dev-certs", "https", "--export-path", certFilePath, "--format", "Pem", "--no-password"],
-			{ stdio: "inherit" }
-		).status
-	) {
-		throw new Error("Could not create certificate.")
-	}
-}
+// if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+// 	if (
+// 		0 !==
+// 		child_process.spawnSync(
+// 			"dotnet",
+// 			["dev-certs", "https", "--export-path", certFilePath, "--format", "Pem", "--no-password"],
+// 			{ stdio: "inherit" }
+// 		).status
+// 	) {
+// 		throw new Error("Could not create certificate.")
+// 	}
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -47,15 +47,15 @@ export default defineConfig({
 	},
 	server: {
 		proxy: {
-			"^/weatherforecast": {
-				target: "https://localhost:7049/",
+			"^/api/v1": {
+				target: "https://localhost:5333/",
 				secure: false,
 			},
 		},
-		port: 5173,
-		https: {
-			key: fs.readFileSync(keyFilePath),
-			cert: fs.readFileSync(certFilePath),
-		},
+		port: 8080,
+		// https: {
+		// 	key: fs.readFileSync(keyFilePath),
+		// 	cert: fs.readFileSync(certFilePath),
+		// },
 	},
 })
